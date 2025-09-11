1755d103d893f8ba41368826917b4dd70a74a13d
Zack <168221270+Zval321@users.noreply.github.com>
2025-09-10 20:30:30 -0500

    updates to footer styling

--- src/screens/Footer/Footer.tsx
+++ src/screens/Footer/Footer.tsx
@@ -1,9 +1,21 @@
 import './Footer.css'
+import { useNavigate } from 'react-router-dom'
 
 export function Footer() {
+  const navigate = useNavigate()
   return (
     <div className='footer-container'>
-      <h1>Hello</h1>
+
+      <div className='footer-left-side'>
+        <h1 onClick={() => navigate('/')}>Login</h1>
+        <h1 onClick={() => navigate('/recipes')}>Recipes</h1>
+        <h1 onClick={() => navigate('/favorites')}>Favorites</h1>
+        <h1 onClick={() => navigate('/story')}>Story</h1>
+      </div>
+
+      <div className='footer-right-side'>
+        <h1>© 2024 All Rights Reserved</h1>
+      </div>
     </div>
   )
 }
