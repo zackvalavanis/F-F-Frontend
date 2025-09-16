dc06aa564378bcd86fdcdfc2fe00aed4b9c84d55
Zack <168221270+Zval321@users.noreply.github.com>
2025-09-15 20:48:32 -0500

    can retrieve recipes from the frontend

--- src/screens/Recipes/Recipes.tsx
+++ src/screens/Recipes/Recipes.tsx
@@ -1,4 +1,34 @@
+import { useState, useEffect } from "react"
+
+interface Recipe {
+  id: number
+  name: string
+}
+
 export function Recipes() {
+  const [recipe, setRecipe] = useState<Recipe[]>([])
+  const api = import.meta.env.VITE_BACKEND
+  console.log(api)
+
+  const handleIndex = async () => {
+    try {
+      const res = await fetch(api)
+
+      const data = await res.json()
+      setRecipe(data)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    handleIndex()
+  }, [])
+
+
+
+
   return (
     <div>
       <h1>Recipes</h1>
