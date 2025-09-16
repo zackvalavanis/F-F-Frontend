771048f2f3b1933a6b0d4716e35a7929a458a745
Zack <168221270+Zval321@users.noreply.github.com>
2025-09-15 21:41:24 -0500

    added a loading icon

--- src/screens/Recipes/Recipes.tsx
+++ src/screens/Recipes/Recipes.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import loadingGif from '../../assets/Loading_icon.gif'
 
 interface Recipe {
   id: number
@@ -16,13 +17,16 @@ interface Recipe {
 export function Recipes() {
   const [recipes, setRecipes] = useState<Recipe[]>([])
   const api = import.meta.env.VITE_BACKEND
+  const [loading, setLoading] = useState(false)
   console.log(api)
 
   const handleIndex = async () => {
+    setLoading(true)
     try {
       const res = await fetch(api)
 
       const data = await res.json()
+      setLoading(false)
       setRecipes(data)
       console.log(data)
     } catch (error) {
@@ -34,7 +38,7 @@ export function Recipes() {
     handleIndex()
   }, [])
 
-
+  if (loading) return <div><img src={loadingGif} alt="Loading..." /></div>
 
 
   return (
