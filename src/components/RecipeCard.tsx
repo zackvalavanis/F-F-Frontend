702f2ce265e216b15a24aee361a1984114b8df1f
Zack <168221270+Zval321@users.noreply.github.com>
2025-11-02 18:42:26 -0600

    updates

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -12,7 +12,7 @@ import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+// import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, Tooltip } from '@mui/material';
